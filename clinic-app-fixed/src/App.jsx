98f69d308577786a508724b06e132f061b1352fc
dbfl0729-pixel <225632749+dbfl0729-pixel@users.noreply.github.com>
2026-03-05 02:02:30 +0900

    재고

---
+++ clinic-app-fixed/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+export default function App(){
+  return(
+    <div style={{fontFamily:"sans-serif",padding:40}}>
+      <h1>클리닉 상담 시스템</h1>
+      <p>가격표 / 재고 / 상담 구조 기본 베이스</p>
+      <ul>
+        <li>가격표 프로그램</li>
+        <li>단품 레이저 테이블</li>
+        <li>재고 관리 구조</li>
+      </ul>
+    </div>
+  )
+}
