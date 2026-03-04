c1a9f77fdde3837d0be48ea3e75d7f2780f991e3
dbfl0729-pixel <225632749+dbfl0729-pixel@users.noreply.github.com>
2026-03-04 15:26:58 +0900

    재고

--- 재고관리/sw.js
+++ 재고관리/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "inventory-cache-v1";
+const CACHE = "inventory-cache-v2";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
@@ -8,15 +8,13 @@ self.addEventListener("install", (e) => {
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
